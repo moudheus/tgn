714fdf7220eeb3cff962543162371fde47dfbdf3
Marc van Oudheusden <moudheus@amazon.com>
2022-12-08 18:29:17 +0000

    Cleanup

--- run.sh
+++ run.sh
@@ -1,26 +1,28 @@
-# lastfm mooc wikipedia reddit
-# q1, q2 = 0.70 0.85
+# Datasets : lastfm mooc wikipedia reddit
+
+# Parameters
+# e  : experiment name
+# q1 : quantile for end of train, default 0.70 
+# q2 : quantile for end of validation, default 0.85
 
 e=9095
+q1=0.90
+q2=0.95
+
 
 for dataset in lastfm mooc wikipedia reddit
 do
-    for q1 in 0.90
-    do
-        for q2 in 0.95
-        do
-            #python utils/preprocess_data.py --data ${dataset} --bipartite
-            python train_self_supervised.py --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --prefix ${e}-tgn
-            python predict.py               --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --prefix ${e}-tgn 
+    python utils/preprocess_data.py --data ${dataset} --bipartite
+    
+    python train_self_supervised.py --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --prefix ${e}-tgn
+    python predict.py               --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --prefix ${e}-tgn 
             
-#            python train_self_supervised.py --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --memory_updater rnn --embedding_module time --prefix ${e}-jodie
-#            python predict.py               --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --memory_updater rnn --embedding_module time --prefix ${e}-jodie
+    python train_self_supervised.py --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --memory_updater rnn --embedding_module time --prefix ${e}-jodie
+    python predict.py               --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --memory_updater rnn --embedding_module time --prefix ${e}-jodie
 
-#            python train_self_supervised.py --data ${dataset} --q1 ${q1} --q2 ${q2} --n_layer 2 --n_degree 20 --prefix ${e}-tgat
-#            python predict.py               --data ${dataset} --q1 ${q1} --q2 ${q2} --n_layer 2 --n_degree 20 --prefix ${e}-tgat
+    python train_self_supervised.py --data ${dataset} --q1 ${q1} --q2 ${q2} --n_layer 2 --n_degree 20 --prefix ${e}-tgat
+    python predict.py               --data ${dataset} --q1 ${q1} --q2 ${q2} --n_layer 2 --n_degree 20 --prefix ${e}-tgat
 
-#            python train_self_supervised.py --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --memory_updater rnn --dyrep --use_destination_embedding_in_message --prefix ${e}-dyrep
-#            python predict.py               --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --memory_updater rnn --dyrep --use_destination_embedding_in_message --prefix ${e}-dyrep
-        done
-    done
+    python train_self_supervised.py --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --memory_updater rnn --dyrep --use_destination_embedding_in_message --prefix ${e}-dyrep
+    python predict.py               --data ${dataset} --q1 ${q1} --q2 ${q2} --use_memory --memory_updater rnn --dyrep --use_destination_embedding_in_message --prefix ${e}-dyrep
 done
